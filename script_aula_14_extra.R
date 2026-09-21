c65e16af3ee5f58345f70704c615b99fccfbfaac
VitorEBFerreira <ferreiravitor@id.uff.br>
2026-09-21 09:35:22 -0300

    script - tarefa 1

--- script_aula_14_extra.R
+++ script_aula_14_extra.R
@@ -3,6 +3,15 @@
 
 # Tarefa 1: Leitura do banco de dados banco 2 = SINASC.csv com o nome de dados_aula14
 # Ler o arquivo, verificar estrutura dos dados e dar uma olhada nos dados
+dados_aula14 = read.csv("banco 2 SINASC.csv",
+                        header = T,
+                        sep = ";",
+                        stringsAsFactors = F)
+dim(dados_aula14)
+names(dados_aula14)
+str(dados_aula14)
+summary(dados_aula14)
+View(dados_aula14)
 
 # Ao terminar a Tarefa 1 commit com a mensagem " script - tarefa 1" e envie para o repositório Aula_14_Extra
 
